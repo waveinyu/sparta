e33cc4927d895270749bb7042eeb41246bac6309
waveinyu <99732695+waveinyu@users.noreply.github.com>
2022-06-08 09:01:56 +0900

    Create app.py

---
+++ projects/mars/app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request, jsonify
+app = Flask(__name__)
+
+from pymongo import MongoClient
+client = MongoClient('mongodb+srv://test:sparta@cluster0.sis7sux.mongodb.net/Cluster0?retryWrites=true&w=majority')
+db = client.dbsparta
+
+@app.route('/')
+def home():
+   return render_template('index.html')
+
+@app.route("/mars", methods=["POST"])
+def web_mars_post():
+    name_receive = request.form['name_give'] # sample_give를 찾는다
+    address_receive = request.form['address_give'] # sample_give를 찾는다
+    size_receive = request.form['size_give'] # sample_give를 찾는다
+    doc = {
+        'name':name_receive,
+        'address':address_receive,
+        'size':size_receive
+    }
+    db.mars.insert_one(doc)
+
+    return jsonify({'msg': '주문 완료!'}) # 메시지를 내려준다
+
+@app.route("/mars", methods=["GET"])
+def web_mars_get():
+    order_list = list(db.mars.find({}, {'_id': False}))
+    return jsonify({'orders': order_list})
+
+if __name__ == '__main__':
+   app.run('0.0.0.0', port=5000, debug=True)
