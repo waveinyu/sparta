86b4df37874306e00facdc8db39f992d91f0acc6
waveinyu <99732695+waveinyu@users.noreply.github.com>
2022-06-08 17:29:54 +0900

    Update app.py

--- projects/movie/app.py
+++ projects/movie/app.py
@@ -41,7 +41,9 @@ def movie_post():
 
 @app.route("/movie", methods=["GET"])
 def movie_get():
-    return jsonify({'msg':'GET 연결 완료!'})
+    movie_list = list(db.movies.find({}, {'_id': False}))
+
+    return jsonify({'movies':movie_list})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
