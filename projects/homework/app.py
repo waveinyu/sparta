b53938db0f6743aa536bd1c3f89661f495ce618e
waveinyu <99732695+waveinyu@users.noreply.github.com>
2022-06-10 20:59:28 +0900

    Update app.py

--- projects/homework/app.py
+++ projects/homework/app.py
@@ -2,31 +2,30 @@ from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
 from pymongo import MongoClient
-client = MongoClient("내 URL")
-db = client.sparta
+client = MongoClient('mongodb+srv://test:sparta@cluster0.sis7sux.mongodb.net/Cluster0?retryWrites=true&w=majority')
+db = client.dbsparta
 
 @app.route('/')
 def home():
-   return render_template('index.html')
+    return render_template('index.html')
 
 @app.route("/homework", methods=["POST"])
 def homework_post():
-    name_receive = request.form["name_give"]
-    comment_receive = request.form["comment_give"]
+    name_receive = request.form['name_give']
+    comment_receive = request.form['comment_give']
 
     doc = {
-        'name': name_receive,
-        'comment': comment_receive
+        'name':name_receive,
+        'comment':comment_receive
     }
-
     db.homework.insert_one(doc)
+
     return jsonify({'msg':'응원 완료!'})
 
 @app.route("/homework", methods=["GET"])
 def homework_get():
-    comment_list = list(db.homework.find({},{'_id':False}))
+    comments_list = list(db.homework.find({}, {'_id': False}))
     return jsonify({'comments':comment_list})
 
-
 if __name__ == '__main__':
-   app.run('0.0.0.0', port=5000, debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)
